5f0a83da0439eb9dc85e44b1d428aa3513a42475
mmswflow-upb <mmswflow@gmail.com>
2026-04-09 05:13:25 +0300

    Initial commit

---
+++ src/main/java/org/bighw1/big_hw_1/BigHw1Application.java
@@ -0,0 +1,13 @@
+package org.bighw1.big_hw_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigHw1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigHw1Application.class, args);
+    }
+
+}
